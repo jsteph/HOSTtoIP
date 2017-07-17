d5545fd287c5b6047e7f4f745696c8137589bd31
Jacob Stephens <dreamofmemes42069@gmail.com>
2017-07-17 01:03:52 -0500

    HOSTtoIP (WINDOWS)
    To Execute: "python3 HOSTtoIP.py"

--- HOSTtoIP.py
+++ HOSTtoIP.py
@@ -1,15 +1,16 @@
+
 import socket
 import os
 import time
 def Main():
-	os.system('clear')
+	os.system('cls')
 	s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-	print("\033[1;31mHOSTtoIP\033[0m\n")
-	host = input("\033[31m> \033[0m")
+	print("HOSTtoIP")
+	host = input("> ")
 	ip = socket.gethostbyname(host)
-	print("\n"+"the ip of \033[1;31m"+host+"\033[0m is \033[1;31m"+ip+"\033[0m\n")
+	print("\n"+"the ip of "+host+" is "+ip+"\n")
 	time.sleep(1)
-	print("\033[1;32myes\033[0m / \033[1;31mno\033[0m")
+	print("yes / no")
 	option = input("find another ip? > ")
 	if option == "yes":
 		Main()
@@ -17,7 +18,7 @@ def Main():
 		exit()
 	else:
 		time.sleep(1)
-		print("\033[1;31mERROR. NOW CLOSING\033[0m")
+		print("ERROR. NOW CLOSING")
 		exit()
 if __name__ == '__main__':
 	Main()
